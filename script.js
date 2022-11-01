a1867d241e0f301ec080cfe6354b7c8f9fb282e7
Eric Nielsen <eniel662@mtroyal.ca>
2022-11-01 13:33:33 -0600

    Added autocomplete, disabling of inputs.

--- script.js
+++ script.js
@@ -2,31 +2,141 @@
  * This page will contain the code controlling the entire single page application
  * If it ends up becoming too long, I may split it up into seperate sections
  */
- import songs from "./songs.json" assert { type: "json" };
-
- alert(songs[0]['artist']['name']);
+songs =  JSON.parse(songData);
+console.log(songs);
+// const api_url = "http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php"; // The URL containing the JSON data that will be fetched
+// async function getSongs(){
+//     const response = await fetch(api_url);
+//     const data = await response.json();
+//     console.log(getSongs());
+// }
 // ========================================================== SONG SEARCH PAGE ============================================================== 
+/**
+ * This function loads the select inputs in the song search page. 
+ * @param {*} fieldName the name of the field that we are looking for in the json file
+ */
+const songTitles = getSongTitles(); // Loads all the song titles into an array
+const titleInput = document.getElementById('title-input');
+const resultsBox = document.getElementById('autocomplete-results');
+titleInput.addEventListener('input', autocompleteTitles);
 
+// This function fills an array with the titles of every song in the json file and returns it
+function getSongTitles(){
+    const songTitles = [];
+    for (let song of songs){
+        songTitles.push(song['title']);
+    }
+    return songTitles;
+}
 
-function loadSelectOptions(fieldName){
-    console.log(fieldName);
-    const selectionId = fieldName += '-select';
-    let selectElement = document.getElementById(selectionId);
-    if (selectElement.options.length === 0){ // This should only be run once, when clicked. 
-        for (let currentSong of songs){
-            let text = currentSong[fieldName.trim("-select")]["name"];
-            let value = currentSong[fieldName.trim("-select")]["id"];
-            const songOption = document.createElement("option", text, {value: value});
-            selectElement.appendChild(songOption);
+function autocompleteTitles(){
+    if (this.value.length >= 2){ // Starts using autocomplete after 2 characters
+        const titleMatch = findMatches(this.value, songTitles);
+        console.log(titleMatch);
+        resultsBox.replaceChildren();
+        titleMatch.forEach(songMatch => {
+                let option = document.createElement('option');
+                option.textContent = songMatch;
+                resultsBox.appendChild(option);
+            }
+        )
+    }
+}
+/* Simple function that checks to see if th*/
+function findMatches(word, titles){
+    const currentMatches = [];
+    for(let title of titles){
+        if (String(title).includes(word)){
+            currentMatches.push(title);
+        }
+    }
+    return currentMatches;
+}
+
+/* Add a event listener to every radio button */
+const radioButtons = document.querySelectorAll("input[type='radio']");
+const inputs = document.querySelectorAll("input, select");
+// Adding an event listener for the radio button 
+for (let currentRadioBtn of radioButtons){
+    currentRadioBtn.addEventListener('change', disableInputs);
+}
+/* Disables all of the inputs that aren't associated with the currently selected radio button*/
+function disableInputs(){
+    if (this.checked){
+        let radioType = String(this.id.replace("-radio", ''));
+        console.log(radioType);
+        for (let input of inputs){
+            if (!input.id.includes(radioType) && input.type != "radio"){
+                input.disabled = true;
+            }
+            else if(input.id.includes(radioType)){
+                input.disabled = false;
+            }
         }
     }
 }
 
-function searchDatabase(){
-    
+// This function is responsible for populating the select elements with options in the song search.
+function loadSelectOptions(fieldName){
+    const field = fieldName;
+    const selectionId = fieldName += '-select';
+    let selectElement = document.getElementById(selectionId);
+    let fieldContainer = [];
+    if (selectElement.options.length === 0){ // This should only be run once, when clicked. 
+        console.log("test");
+        for (let currentSong of songs){
+            let optionText = currentSong[field]["name"];
+            if (!fieldContainer.includes(optionText)){ // Ensures that there are no duplicates 
+                fieldContainer.push(optionText);
+                let optionValue = currentSong[field]["id"];
+                const songOption = document.createElement("option");
+                songOption.text = fieldContainer[fieldContainer.length - 1]; // adds the element that we just pushed into the array
+                songOption.value = optionValue;
+                selectElement.appendChild(songOption);
+            }
+        }
+    }
 }
-document.getElementById("artist-select").addEventListener("click", loadSelectOptions("artist"));
-document.getElementById("genre-select").addEventListener("click", loadSelectOptions("genre"));
+// Load the select elements with data upon clicking.
+document.getElementById("genre-select").addEventListener('click', loadSelectOptions("genre"));
+document.getElementById("artist-select").addEventListener('click', loadSelectOptions("artist"));
+
+
+document.getElementById("submit-btn").addEventListener('click', searchJSON())
+function searchJSON(value, radioSelection){
+    const results = [];
+    let elemName = radioSelection.replace("-radio", "");
+    for (let song of songs){
+        if (elemName == 'title'){
+
+        }
+        else if (elemName == 'artist'){
+
+        }
+        else if (elemName == 'genre'){
+
+        }
+    }
+    switch(radioSelection.toLowerCase()){
+        case "title-radio":
+            for (let song of songs){
+                if (song.title == value){
+
+                }
+            }
+            break;
+        case "artist-radio":
+            break;
+        case "genre-radio":
+            break;
+        case "year-radio":
+            break;
+        case "popularity-radio":
+            break;
+        default:
+            console.log("Invisible radio button pressed?");
+    }
+}
 
 // ======================================================== SONG INFORMATION PAGE =========================================================== 
 
