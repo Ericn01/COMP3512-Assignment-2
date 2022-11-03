11f0b4ffbcdfd72f0b55215ceaddee793573a2ec
Eric Nielsen <nielseneric2001@gmail.com>
2022-11-03 12:12:17 -0600

    song search and table display functional.

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ songs =  JSON.parse(songData);
  * @param {*} fieldName the name of the field that we are looking for in the json file
  */
 const songTitles = getSongTitles(); // Loads all the song titles into an array
-const titleInput = document.getElementById('title-input');
-const resultsBox = document.getElementById('autocomplete-results');
+const titleInput = document.querySelector('#title-input');
+const resultsBox = document.querySelector('#autocomplete-results');
 titleInput.addEventListener('input', autocompleteTitles);
 
 // This function fills an array with the titles of every song in the json file and returns it
@@ -28,9 +28,9 @@ function getSongTitles(){
     }
     return songTitles;
 }
-
+// This function appends the autocomplete matches to the datalist
 function autocompleteTitles(){
-    if (this.value.length >= 2){ // Starts using autocomplete after 2 characters
+    if (this.value.length >= 1){ // Starts using autocomplete after 1 character has been typed
         const titleMatch = findMatches(this.value, songTitles);
         resultsBox.replaceChildren();
         titleMatch.forEach(songMatch => {
@@ -41,6 +41,7 @@ function autocompleteTitles(){
         )
     }
 }
+
 /* Simple function that checks to see if th*/
 function findMatches(word, titles){
     const currentMatches = [];
@@ -94,8 +95,8 @@ function loadSelectOptions(fieldName){
     }
 }
 // Load the select elements with data upon clicking.
-document.getElementById("genre-select").addEventListener('click', loadSelectOptions("genre"));
-document.getElementById("artist-select").addEventListener('click', loadSelectOptions("artist"));
+document.querySelector("#genre-select").addEventListener('click', loadSelectOptions("genre"));
+document.querySelector("#artist-select").addEventListener('click', loadSelectOptions("artist"));
 
     function populateRow(parentElement, attribute, songObj){
         const cellElement = document.createElement("td");
@@ -112,6 +113,11 @@ document.getElementById("artist-select").addEventListener('click', loadSelectOpt
         parentElement.appendChild(cellElement);
     }
     function populateSongs(results){ // Should run in O(nlog(n)) time since the inner loop has a fixed length. Not great, but could be worse
+        const resultsBody = document.querySelector("#results-body")
+        // Clears the table if elements are already present
+        if (resultsBody.hasChildNodes()){
+            resultsBody.innerHTML = "";
+        }
         const labels = ['title', 'artist', 'year', 'genre', 'details'];
         let songRow;
         for(let song of results){
@@ -119,16 +125,16 @@ document.getElementById("artist-select").addEventListener('click', loadSelectOpt
             for (let i = 0; i < labels.length; i++){
                 populateRow(songRow, labels[i], song);
             }
-            document.getElementById('results-body').appendChild(songRow); // Appends the current row to the table body
-        }
+        resultsBody.appendChild(songRow); // Appends the current row to the table body
     }
-    populateSongs(songs);
-    document.getElementById("clear-btn").addEventListener("click", (e) => {
+}
+populateSongs(songs);
+    document.querySelector("#clear-btn").addEventListener("click", (e) => {
         for (let input of inputs){
             input.textContent = "";
         }
     });
-    document.getElementById("submit-btn").addEventListener("click", (e) => {
+    document.querySelector("#submit-btn").addEventListener("click", (e) => {
             // retrieving data from the button
             let id = e.target.getAttribute('data-id');
             // get song object from the button
@@ -136,18 +142,18 @@ document.getElementById("artist-select").addEventListener('click', loadSelectOpt
     );
 
     // Form data handling section
-    document.addEventListener("DOMContentLoaded", ()=> { // Populates the results table with all songs
-        const songForm = document.getElementById('song-form');
-        songForm.addEventListener('submit', formProcessing);
-    });
+    // Populates the results table with all songs
+    const submitBtn = document.querySelector('#submit-btn');
+    submitBtn.addEventListener('click', formProcessing);
+    
+    // appends the given input value to the given array 
     function pushInputValue(inputName, arr){
-        console.log(inputName);
-        const value = document.getElementById(inputName).value;
+        const value = document.querySelector(`#${inputName}`).value;
         arr.push(value);
     }
     /**
-     * Searches for and returns the id value of the radio button that is checked.
-     * @returns the id of the form radio button that is currently selected
+     * Searches for and returns the id value of the * radio button that is checked.
+     * @returns the id of the form radio button that is * currently selected
      */
     function getSelectedRadioButtonId(){
         let selectedRadioId;
@@ -157,91 +163,90 @@ document.getElementById("artist-select").addEventListener('click', loadSelectOpt
             }
         }
         return selectedRadioId;
-    }
+    }   
     // This function is responsible for processing the data sent through the form
     function formProcessing(){
-        const selectedRadioId = getSelectedRadioButtonId();
-        const searchParam = []; // Array containing the search parameters 
-        let searchAttribute = "";
+        // Prevents a page reload when we submit the form.
+        document.querySelector("#song-form").addEventListener('submit', e => e.preventDefault());        const selectedRadioId = getSelectedRadioButtonId();
+        const searchParameters = []; // Array containing the search parameters 
+        let searchAttribute = ""; // The attribute that the search is going by
+        // Different logic will be applied depending on the selected radio button 
         switch(selectedRadioId){
             case "title-radio":
-                pushInputValue("title-input", searchParam);
+                pushInputValue("title-input", searchParameters);
                 searchAttribute = "title";
                 break;
             case "artist-radio":
-                pushInputValue("artist-select", searchParam);
+                pushInputValue("artist-select", searchParameters);
                 searchAttribute = "artist";
                 break;
             case "genre-radio":
-                pushInputValue("genre-select", searchParam);
+                pushInputValue("genre-select", searchParameters);
                 searchAttribute = "genre";
                 break;
             case "year-radio":
-                pushInputValue("year-less-input", searchParam);
-                pushInputValue("year-greater-input", searchParam);
+                pushInputValue("year-less-input", searchParameters);
+                pushInputValue("year-greater-input", searchParameters);
                 searchAttribute = "year"
                 break;
             case "popularity-radio":
-                pushInputValue("popularity-less-input", searchParam);
-                pushInputValue("popularity-greater-input", searchParam);
+                pushInputValue("popularity-less-input", searchParameters);
+                pushInputValue("popularity-greater-input", searchParameters);
                 searchAttribute = "popularity";
                 break;
             default:
         }
-        const searchResults = findResults(searchParam, searchAttribute, songs); // Returns the results of the search query
+        const searchResults = findResults(searchParameters, searchAttribute, songs); // Returns the results of the search query
         populateSongs(searchResults);
     }
-
     function findResults(valuesArr, searchAttribute, songObj){
         const results = [];
-        if (valuesArr.length == 1){
-            let userValue = valuesArr[0];
-            let subSearchAttribute = "name"; // Remains the same for all attributes with 
-            for (let song of songObj){
-                if (song[searchAttribute][subSearchAttribute].includes(userValue)){
-                    results.push(song);
-                }
+        const userValue = valuesArr[0];
+        // Loop through the song objects array
+        let count = 0;
+        for (song of songObj){
+            if (searchAttribute == 'title' && String(song['title']).includes(userValue)){
+                results.push(song);
             }
-        }
-        else if (valuesArr.length == 2){
-            const upperBound = Number(valuesArr[1]); // The upper boundary of the two given values
-            const lowerBound = Number(valuesArr[0]); // The lower bound of the two given values
-            for (let song of songs){
+            else if ((searchAttribute =='artist' || searchAttribute == 'genre') && (song[searchAttribute]['name'].includes)(userValue)){
+                results.push(song);
+            }
+            else if (searchAttribute == 'popularity' || searchAttribute == 'year'){
+                const upperBound = Number(valuesArr[1]); // The upper boundary of the two given values
+                const lowerBound = Number(valuesArr[0]); // The lower bound of the two given values
                 if (searchAttribute == "popularity"){ 
                     const songPopularity = Number(song["details"][searchAttribute]); // The current song's popularity
                     if (songPopularity >= lowerBound && songPopularity <= upperBound){
-                        results.push(song); // push the song if the given constrains are met
+                        results.push(song);
                     }
                 }
                 else{ // Search attribute is year
                     const songYear = Number(song[searchAttribute]); // The current song's popularity
                     if (songYear >= lowerBound && songYear <= upperBound){
-                        results.push(song); // push the song if the given constrains are met
+                        results.push(song);
                     }
                 }
             }
-        }
-        else{
-            console.log("Oops, you have no search parameters! No songs will be found");
+            else if (searchAttribute == ""){
+                console.log("No search attribute has been chosen");
+            }
+            count++;
         }
         return results;
     }
 
-
-
 // ======================================================== SONG INFORMATION PAGE =========================================================== 
+const data = {
+    labels: [
+    'Danceability', 'Energy', 'Speechiness', 'Acousticness', 'Liveness', 'Valence']
 
-let ctx = document.getElementById("song-chart").getContext('2d');
-let chart = new Chart(ctx, {
-    type: 'radar',
-    data:{
-        labels: ['Danceability', 'Energy', 'Speechiness', 'Acousticness', 'Liveness', 'Valence'],
-        datasets: [{
-            data: [80, 55, 20, 90, 12, 25]
-        }]
-    }, 
-    options: options
-});
+};
+let ctx = document.querySelector("#song-chart").getContext('2d');
+// let chart = new Chart(ctx, {
+//     type: 'radar',
+//     data: data,
+//     options: options
+// });
 
 
 // ======================================================== PLAYLIST INFO PAGE ============================================================== 
